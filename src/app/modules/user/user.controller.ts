97de56ab6c4c5ecd76caeb93f7a92e98d3579f9b
muntasirmoin <muntasirmoinchowdhury099@gmail.com>
2025-07-17 18:01:54 +0600

    part-4

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -7,6 +7,7 @@ import { StatusCodes as httpStatus } from "http-status-codes";
 import { UserServices } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
+import { JwtPayload } from "jsonwebtoken";
 
 const createUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -61,7 +62,11 @@ const updateUser = catchAsync(
     const verifiedToken = req.user;
 
     const payload = req.body;
-    const user = await UserServices.updateUser(userId, payload, verifiedToken);
+    const user = await UserServices.updateUser(
+      userId,
+      payload,
+      verifiedToken as JwtPayload
+    );
 
     // res.status(httpStatus.CREATED).json({
     //     message: "User Created Successfully",
