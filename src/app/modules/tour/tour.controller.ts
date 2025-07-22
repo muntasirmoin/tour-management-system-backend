175b9054ae91d2cd763a34ff7ab62bb257f5728a
muntasirmoin <muntasirmoinchowdhury099@gmail.com>
2025-07-22 13:06:48 +0600

    part-6

--- src/app/modules/tour/tour.controller.ts
+++ src/app/modules/tour/tour.controller.ts
@@ -25,6 +25,16 @@ const getAllTours = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const getSingleTour = catchAsync(async (req: Request, res: Response) => {
+  const slug = req.params.slug;
+  const result = await TourService.getSingleTour(slug);
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Tour retrieved successfully",
+    data: result,
+  });
+});
 const updateTour = catchAsync(async (req: Request, res: Response) => {
   const result = await TourService.updateTour(req.params.id, req.body);
   sendResponse(res, {
@@ -34,7 +44,6 @@ const updateTour = catchAsync(async (req: Request, res: Response) => {
     data: result,
   });
 });
-
 const deleteTour = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
   const result = await TourService.deleteTour(id);
@@ -45,8 +54,21 @@ const deleteTour = catchAsync(async (req: Request, res: Response) => {
     data: result,
   });
 });
+const getSingleTourType = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const result = await TourService.getSingleTourType(id);
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Tour type retrieved successfully",
+    data: result,
+  });
+});
 const getAllTourTypes = catchAsync(async (req: Request, res: Response) => {
-  const result = await TourService.getAllTourTypes();
+  const query = req.query;
+  const result = await TourService.getAllTourTypes(
+    query as Record<string, string>
+  );
   sendResponse(res, {
     statusCode: 200,
     success: true,
@@ -92,9 +114,11 @@ export const TourController = {
   createTour,
   createTourType,
   getAllTourTypes,
+  getSingleTourType,
   deleteTourType,
   updateTourType,
   getAllTours,
+  getSingleTour,
   updateTour,
   deleteTour,
 };
