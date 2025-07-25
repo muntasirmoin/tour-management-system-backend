bc57a2412b000a0c61adef5dec00b22a078a2bc2
muntasirmoin <muntasirmoinchowdhury099@gmail.com>
2025-07-25 11:57:21 +0600

    part-7

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -17,6 +17,7 @@ router.get(
   checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
   UserControllers.getAllUsers
 );
+router.get("/me", checkAuth(...Object.values(Role)), UserControllers.getMe);
 router.get(
   "/:id",
   checkAuth(...Object.values(Role)),
